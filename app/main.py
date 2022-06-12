61a625382424fd3d074bc5c778d47d8ff96d96f2
Jason Leaver <leaver2000@gmail.com>
2022-06-12 15:38:23 -0500

    .

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@
 import os
 
 from fastapi import FastAPI,Request
+import os
+from pathlib import Path
 
+cron_path = Path(os.getcwd(), "app", "cron.log")
 
 DATAPATH = os.getenv("DIRECTORY","/media/external/data")
 
@@ -15,3 +18,9 @@ def health():
 @app.get("/headers")
 def headers(request:Request):
     return request.headers
+
+
+@app.get("/cron")
+def cron():
+    with cron_path.open("rt") as cron:
+        return {"cron_state": cron.read()}
